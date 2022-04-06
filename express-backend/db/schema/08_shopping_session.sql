4dab7f7323bbbdcc9a58379030951d5dcd2ec88b
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-04-06 12:53:04 -0400

    add shopping_session table to schema

---
+++ express-backend/db/schema/08_shopping_session.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS shopping_session CASCADE;
+
+CREATE TABLE shopping_session(
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  total VARCHAR(255) NOT NULL
+);

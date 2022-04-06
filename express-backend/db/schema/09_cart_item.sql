cd0ab4aa3544df94b7e38f745a28e3727b12ccf6
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-04-06 12:57:45 -0400

    add cart_item table to schema

---
+++ express-backend/db/schema/09_cart_item.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS cart_item CASCADE;
+
+CREATE TABLE cart_item(
+  id SERIAL PRIMARY KEY NOT NULL,
+  session_id INTEGER REFERENCES shopping_session(id),
+  product_id INTEGER REFERENCES products(id),
+  total VARCHAR(255) NOT NULL
+);

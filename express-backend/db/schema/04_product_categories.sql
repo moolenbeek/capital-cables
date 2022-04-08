7a26e9a3776e1e76021deb86e65f1bd53c5079b5
Eric Moolenbeek <emoolenbeek@gmail.com>
2022-04-08 19:12:53 +0000

    update product categories schema

--- express-backend/db/schema/04_product_categories.sql
+++ express-backend/db/schema/04_product_categories.sql
@@ -3,5 +3,7 @@ DROP TABLE IF EXISTS product_categories CASCADE;
 CREATE TABLE product_categories(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL
+  description VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP
 );
